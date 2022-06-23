cdb4519c2385bd4920a0b29c10b9b8feb05de73b
Mickael Bobovitch <mickael.bobovitch@epita.fr>
2022-06-23 16:59:19 +0200

    can stream

--- hikvision/hikvision.py
+++ hikvision/hikvision.py
@@ -1,2 +1,212 @@
-import logger
-import settings
+from logger import logger
+from settings import SETTINGS
+import requests
+import xmltodict
+from typing import List
+import cv2
+import numpy as np
+
+
+class HikvisionCamera:
+    def __init__(self, id, ip, port, name, username) -> None:
+        self.id = id
+        self.ip = ip
+        self.port = port
+        self.name = name
+        self.username = username
+        self.cap = None
+
+    def __repr__(self) -> str:
+        return "HikvisionCamera(id={}, ip={}, port={}, name={}, username={})".format(
+            self.id, self.ip, self.port, self.name, self.username
+        )
+
+    def __str__(self) -> str:
+        return "HikvisionCamera(id={}, ip={}, port={}, name={}, username={})".format(
+            self.id, self.ip, self.port, self.name, self.username
+        )
+
+    @property
+    def stream_url(self) -> str:
+        return "rtsp://{}:{}@{}:{}/ISAPI/Streaming/channels/{}0{}".format(
+            SETTINGS["username"],
+            SETTINGS["password"],
+            SETTINGS["ip"],
+            SETTINGS["rtsp-port"],
+            self.id,
+            SETTINGS["channel"],
+        )
+
+    @property
+    def api_url(self) -> str:
+        return "http://{}:{}@{}:{}/ISAPI/Streaming/channels/{}0{}".format(
+            SETTINGS["username"],
+            SETTINGS["password"],
+            SETTINGS["ip"],
+            SETTINGS["http-port"],
+            self.id,
+            SETTINGS["channel"],
+        )
+
+    @property
+    def is_online(self) -> bool:
+        try:
+            response = requests.get(self.api_url)
+            return response.status_code == 200
+        except requests.exceptions.RequestException as e:
+            logger.error(e)
+            return False
+
+    @property
+    def picture(self) -> np.ndarray:
+        try:
+            response = requests.get(self.api_url + "/picture")
+            if response.status_code == 200:
+                return cv2.imdecode(
+                    np.frombuffer(response.content, np.uint8), cv2.IMREAD_COLOR
+                )
+            else:
+                logger.error("Error getting image from {}".format(self.api_url))
+                return None
+        except requests.exceptions.RequestException as e:
+            logger.error(e)
+            return None
+
+    def init_capture(self) -> bool:
+        self.cap = cv2.VideoCapture(self.stream_url)
+        return self.cap.isOpened()
+
+    def release_capture(self) -> None:
+        if self.cap.isOpened():
+            self.cap.release()
+
+    @property
+    def frame(self) -> np.ndarray:
+        if self.cap.isOpened():
+            ret, frame = self.cap.read()
+            if ret:
+                return frame
+            else:
+                logger.error("Error getting frame from {}".format(self.api_url))
+                return None
+        else:
+            logger.error("Camera {} is not initialized".format(self.id))
+            return None
+
+
+def get_cameras() -> List[HikvisionCamera]:
+    """
+    Get all cameras from the hikvision server
+    """
+    url = "http://{}:{}@{}:{}/ISAPI/ContentMgmt/InputProxy/channels".format(
+        SETTINGS["username"],
+        SETTINGS["password"],
+        SETTINGS["ip"],
+        SETTINGS["http-port"],
+    )
+
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        logger.error("Failed to get cameras")
+        return None
+
+    xml = response.text
+
+    try:
+        data = xmltodict.parse(xml)
+        cameras = data["InputProxyChannelList"]["InputProxyChannel"]
+    except Exception as e:
+        logger.error("Failed to parse xml")
+        cameras = []
+        return cameras
+
+    hikvision_cameras = []
+
+    for camera in cameras:
+        info = camera["sourceInputPortDescriptor"]
+        hikvision_cameras.append(
+            HikvisionCamera(
+                camera["id"],
+                info["ipAddress"],
+                info["managePortNo"],
+                camera["name"],
+                info["userName"],
+            )
+        )
+
+    return hikvision_cameras
+
+
+def get_camera_by_id(id: int) -> HikvisionCamera:
+    """
+    Get camera by id
+    """
+    cameras = get_cameras()
+
+    for camera in cameras:
+        if camera.id == id:
+            return camera
+
+    return None
+
+
+def get_camera_by_name(name: str) -> HikvisionCamera:
+    """
+    Get camera by name
+    """
+    cameras = get_cameras()
+
+    for camera in cameras:
+        if camera.name == name:
+            return camera
+
+    return None
+
+
+def get_camera_by_ip(ip: str) -> HikvisionCamera:
+    """
+    Get camera by ip
+    """
+    cameras = get_cameras()
+
+    for camera in cameras:
+        if camera.ip == ip:
+            return camera
+
+    return None
+
+
+def get_available_cameras() -> List[HikvisionCamera]:
+    """
+    Get all cameras that are online
+    """
+    cameras = get_cameras()
+
+    available_cameras = []
+
+    for camera in cameras:
+        if camera.is_online:
+            available_cameras.append(camera)
+
+    return available_cameras
+
+
+if __name__ == "__main__":
+    cameras = get_available_cameras()
+
+    for camera in cameras:
+        print(camera)
+        camera.init_capture()
+        logger.info("Camera {} is online ({})".format(camera.id, camera.name))
+
+        while True:
+            image = camera.frame
+            if image is not None:
+                cv2.imshow("image", image)
+
+            if cv2.waitKey(1) & 0xFF == ord("q"):
+                break
+        
+        camera.release_capture()
+        cv2.destroyAllWindows()
--- hikvision/settings.py
+++ hikvision/settings.py
@@ -1,34 +1,35 @@
 import json
 import os
 import sys
-import logging
+from logger import logger
 
 SETTINGS = {
     "ip": "192.168.1.1",
-    "http": "8080",
-    "rtsp": "554",
-    "server": "5000",
+    "http-port": "8080",
+    "rtsp-port": "554",
+    "server-port": "5000",
     "username": "username",
     "password": "password",
+    "channel": "2",
 }
 
 if not os.path.exists("settings.json"):
-    logging.info("settings.json not found, creating default settings.json")
+    logger.info("settings.json not found, creating default settings.json")
 
     with open("settings.json", "w") as f:
         f.write(json.dumps(SETTINGS, indent=4))
 
-    logging.info("settings.json created")
-    logging.info("Please edit settings.json and restart")
+    logger.info("settings.json created")
+    logger.info("Please edit settings.json and restart")
 
     sys.exit(0)
 
 else:
-    logging.info("settings.json found")
+    logger.info("settings.json found")
 
     with open("settings.json", "r") as f:
         try:
             SETTINGS = json.loads(f.read())
         except json.decoder.JSONDecodeError:
-            logging.error("settings.json is not valid json")
+            logger.error("settings.json is not valid json")
             sys.exit(1)
